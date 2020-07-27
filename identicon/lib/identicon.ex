3a3ddcdc5a3354b7891a58f13dc3a077fd442377
Victor Rocha <themind1991@MacBook-Pro-de-Victor.local>
2020-07-26 21:40:12 -0300

    Identicon draw image last version

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,7 +9,38 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
 
+  end
+
+  def save_image(img_file, img_name) do
+    File.write("#{img_name}.png", img_file)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({ _value, index }) ->
+      horizontal = rem(index, 5 ) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      botoom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, botoom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map})do 
+    image = :egd.create(250,250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start,stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+    :egd.render(image)
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image)do
